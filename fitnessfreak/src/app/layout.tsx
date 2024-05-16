bf7320e99b146d4604dfbfc9e5ca653f78ea8bf9
awoliope <awoliioop@gmail.com>
2024-05-16 09:49:09 -0700

    semi finished with the backend, started the admin panel

--- fitnessfreak/src/app/layout.tsx
+++ fitnessfreak/src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/Navbar/Navbar";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NavBar />
         {children}
+        <ToastContainer />
       </body>
     </html>
   );
